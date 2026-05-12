36c5ff7bc6d34bf48f3c8dfb7d5dfedc6ae3de5a
AlexGarcia21 <ag7333303@gmail.com>
2026-05-12 16:47:27 -0600

    Estructura de funciones real con archivo de control

--- netlify/functions/index.php
+++ netlify/functions/index.php
@@ -1,20 +1,17 @@
 <?php
-// Esto le indica al navegador que debe renderizar HTML y no descargar el archivo
+// Evita que el navegador descargue el archivo
 header('Content-Type: text/html; charset=utf-8');
 
-// Cargamos el autoloader de Composer
+// Carga el motor de Laravel
 require __DIR__ . '/../../vendor/autoload.php';
-
-// Iniciamos la aplicación Laravel
 $app = require __DIR__ . '/../../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// Capturamos la petición y generamos la respuesta
+// Procesa la petición
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
-
 $kernel->terminate($request, $response);
