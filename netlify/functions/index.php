ea83aa104c3c4465a2e24db5901a8532ddf8376b
AlexGarcia21 <ag7333303@gmail.com>
2026-05-12 15:25:02 -0600

    Final fix:Fix: Definición de directorio de funciones

--- netlify/functions/index.php
+++ netlify/functions/index.php
@@ -1,15 +1,20 @@
 <?php
-// Esto evita que se descargue
+// Esto le indica al navegador que debe renderizar HTML y no descargar el archivo
 header('Content-Type: text/html; charset=utf-8');
 
+// Cargamos el autoloader de Composer
 require __DIR__ . '/../../vendor/autoload.php';
+
+// Iniciamos la aplicación Laravel
 $app = require_once __DIR__ . '/../../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+// Capturamos la petición y generamos la respuesta
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
+
 $kernel->terminate($request, $response);
