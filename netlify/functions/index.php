80be438fa9acfe64cc99c891018a600cdc3825be
AlexGarcia21 <ag7333303@gmail.com>
2026-05-12 15:18:10 -0600

    Final fix:Fix: Definición de directorio de funciones

--- netlify/functions/index.php
+++ netlify/functions/index.php
@@ -1,12 +1,15 @@
 <?php
+// Esto evita que se descargue
 header('Content-Type: text/html; charset=utf-8');
 
 require __DIR__ . '/../../vendor/autoload.php';
 $app = require_once __DIR__ . '/../../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
+
 $response->send();
 $kernel->terminate($request, $response);
